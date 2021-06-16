d2799ec53a5429cd24fb1ac125d254b11dc7c0de
moh52 <mohammedatthar52@gmail.com>
2021-06-16 13:05:17 +0530

    API

--- Upkeep_v3/Upkeep_v3/Custom_Integration/PAZO/Raise_Feedback_Issue.aspx.cs
+++ Upkeep_v3/Upkeep_v3/Custom_Integration/PAZO/Raise_Feedback_Issue.aspx.cs
@@ -17,6 +17,52 @@ namespace Upkeep_v3.Custom_Integration.PAZO
         protected void btnSave_Click(object sender, EventArgs e)
         {
 
+
+
+
+
+
+
+
+
         }
+
+
+        public void Bind_Details()
+        {
+            try
+            {
+                ////int studentId = Request.QueryString["StudentId"];
+                // string BaseURL = string.Empty;
+                // //BaseURL = Convert.ToString(ConfigurationManager.AppSettings["Base_URL"]);
+
+                //// SuccessURL = BaseURL + "Admission/FeePaymentSuccess.aspx?status=0&studentid=" + studentId;
+
+                // //string surl = SuccessURL;
+                // Session.Add("surl", surl);
+
+                // Session["surl"] = SuccessURL;
+
+                // Random r = new Random();
+                // string txnid = "Txn" + r.Next(100, 9999);
+                // Session.Add("txnid", txnid);
+
+                // hdnMerchantKey.Value = Convert.ToString(ConfigurationManager.AppSettings["MERCHANT_KEY"]);
+                // hdnMerchantSalt.Value = Convert.ToString(ConfigurationManager.AppSettings["SALT"]);
+                // hdnTransactionID.Value = "";
+                // hdnAmount.Value = "";
+                // hdnProductInfo.Value = Convert.ToString(ConfigurationManager.AppSettings["ProductInfo"]);
+                // hdnFirstName.Value = "Faisal"; //Convert.ToString(txtFirstname.Text.Trim());
+                // hdnEmailID.Value = ""; //Convert.ToString(txtEmailID.Text.Trim());
+                // hdnMobileNo.Value = ""; //Convert.ToString(txtMobileNo.Text.Trim());
+                //                                   //hdnHash.Value =
+                // Page.ClientScript.RegisterStartupScript(this.GetType(), "CallMyFunction", "generateHash()", true);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
     }
 }
