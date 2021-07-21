db5f6323cecbd50e6ea7d4d5624f6b1f7560514b
Lokesh Devasani <lokesh@compelconsultancy.com>
2021-07-21 13:48:10 +0530

    Feedback Config Design and JS addition

--- Upkeep_v3/Upkeep_v3/Feedback/EventDetails.aspx.designer.cs
+++ Upkeep_v3/Upkeep_v3/Feedback/EventDetails.aspx.designer.cs
@@ -139,24 +139,6 @@ namespace Upkeep_v3.Feedback {
         protected global::System.Web.UI.WebControls.TextBox Location;
         
         /// <summary>
-        /// txtCategory control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText txtCategory;
-        
-        /// <summary>
-        /// dlCategory control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl dlCategory;
-        
-        /// <summary>
         /// Customer1 control.
         /// </summary>
         /// <remarks>
