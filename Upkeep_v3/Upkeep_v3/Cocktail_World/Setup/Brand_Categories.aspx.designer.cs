49d9fbe7cb8a96de13e2975ae877a2401b24562c
Harsh Araple <harsharaple56@gmail.com>
2021-09-15 00:09:01 +0530

    Commit Transfer modual and Other Bugs

--- Upkeep_v3/Upkeep_v3/Cocktail_World/Setup/Brand_Categories.aspx.designer.cs
+++ Upkeep_v3/Upkeep_v3/Cocktail_World/Setup/Brand_Categories.aspx.designer.cs
@@ -49,6 +49,42 @@ namespace Upkeep_v3.Cocktail_World.Setup {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvDept;
         
         /// <summary>
+        /// btn_edit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btn_edit;
+        
+        /// <summary>
+        /// I1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl I1;
+        
+        /// <summary>
+        /// btn_delete control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btn_delete;
+        
+        /// <summary>
+        /// I2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl I2;
+        
+        /// <summary>
         /// btnAddcategory control.
         /// </summary>
         /// <remarks>
